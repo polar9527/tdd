293cf19385bd0f6da62bd1e6275f016317e04d1f
polar9527 <polar9527@163.com>
2017-09-13 19:36:30 +0800

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,32 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
+#
+#try:
+#    browser.get('http://localhost:8000')
+#    
+#    assert 'To-Do' in browser.title, "Browser title was " + browser.title
+#finally:
+#    browser.quit()
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Polar' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+	self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
